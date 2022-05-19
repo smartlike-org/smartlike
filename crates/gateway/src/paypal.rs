2c410dfbeb8afaa8648ed44f0efb0b373c16b6e3
vadim-frolov <vadim.frolov@thank-u.org>
2022-05-19 11:07:07 +0200

    added support for regular PayPal donations

--- crates/gateway/src/paypal.rs
+++ crates/gateway/src/paypal.rs
@@ -4,6 +4,7 @@ use reqwest;
 use reqwest::header::USER_AGENT;
 use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
+use uuid;
 
 // for sandbox testing - static PAYPAL_IPN: &str = "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr";
 static PAYPAL_IPN: &str = "https://ipnpb.paypal.com/cgi-bin/webscr";
@@ -12,14 +13,10 @@ pub async fn parse(
     query_string: String,
     query: web::Query<HashMap<String, String>>,
 ) -> anyhow::Result<DonationReceipt> {
-    let receipt = parse_ipn(&query)?;
+    println!("Received {}", query_string);
     match verify(&query_string).await {
-        Ok(_) => {
-            return Ok(receipt);
-        }
-        Err(e) => {
-            return Err(anyhow::anyhow!("Verification error: {}", e));
-        }
+        Ok(_) => parse_ipn(&query),
+        Err(e) => Err(anyhow::anyhow!("Verification error: {}", e)),
     }
 }
 
@@ -27,40 +24,52 @@ fn parse_ipn(params: &web::Query<HashMap<String, String>>) -> anyhow::Result<Don
     check_required_fields(
         params,
         &[
-            "invoice".to_string(),
-            "item_name".to_string(),
-            "item_number".to_string(),
-            "business".to_string(),
+            "receiver_email".to_string(),
             "payer_status".to_string(),
             "payment_status".to_string(),
             "payment_type".to_string(),
             "mc_gross".to_string(),
             "mc_fee".to_string(),
             "mc_currency".to_string(),
-            "custom".to_string(),
+            "txn_type".to_string(),
+            "txn_id".to_string(),
         ],
     )?;
 
-    assert_parameter(params, "txn_type", "web_accept")?;
     assert_parameter(params, "payment_type", "instant")?;
     assert_parameter(params, "payment_status", "Completed")?;
 
-    let subject: Vec<&str> = params["item_name"].split(' ').collect();
-    if subject.len() != 3 || subject[0] != "Donate" || subject[1] != "to" {
+    if params["txn_type"] != "web_accept"
+        && params["txn_type"] != "recurring_payment"
+        && params["txn_type"] != "send_money"
+    {
         return Err(anyhow::anyhow!(
-            "Wrong item name: {}. Must start with \"Donate to \"",
-            params["item_name"]
+            "Wrong ipn parameter txn_type = \"{}\"",
+            params["txn_type"],
         ));
     }
 
-    let custom: Vec<&str> = params["custom"].split(' ').collect();
-    if custom.len() != 3 {
-        return Err(anyhow::anyhow!(
-            "Failed to parse custom field: {}",
-            params["custom"]
-        ));
+    // Donate+to+4855e1d3-ac4a-f6c4-8e03-f66001cef053+from+256bd4c260ee7d9554cf926a5120d0632b149f54a86ac65b660198b4c42c292d+USD
+    let mut data = "".to_string();
+    if params.contains_key("product_name") && params["product_name"].len() > 100 {
+        data = params["product_name"].to_string();
+    } else if params.contains_key("transaction_subject")
+        && params["transaction_subject"].len() > 100
+    {
+        data = params["transaction_subject"].to_string();
+    } else if params.contains_key("item_name") && params["item_name"].len() > 100 {
+        data = params["item_name"].to_string();
     }
-
+    let custom: Vec<&str> = data.split(' ').collect();
+    if custom.len() != 6
+        || custom[0] != "Donate"
+        || custom[1] != "to"
+        || custom[3] != "from"
+        || uuid::Uuid::parse_str(custom[2]).is_ok() == false
+        || custom[4].len() < 64
+    {
+        return Err(anyhow::anyhow!("Not a Smartlike notification: {}.", data));
+    }
     let f_amount = params["mc_gross"]
         .parse::<f64>()
         .map_err(|_err| anyhow::anyhow!("failed to parse mc_gross parameter"))?;
@@ -72,17 +81,18 @@ fn parse_ipn(params: &web::Query<HashMap<String, String>>) -> anyhow::Result<Don
     let now = SystemTime::now();
     let ts = now.duration_since(UNIX_EPOCH).unwrap().as_secs() as u32;
 
+    // Donate+to+4855e1d3-ac4a-f6c4-8e03-f66001cef053+from+256bd4c260ee7d9554cf926a5120d0632b149f54a86ac65b660198b4c42c292d+USD
     Ok(DonationReceipt {
-        donor: custom[0].to_string(),
-        recipient: custom[1].to_string(),
-        channel_id: custom[1].to_string(),
-        alias: subject[2].to_string(),
-        id: params["invoice"].to_string(),
-        address: params["business"].to_string(),
+        donor: custom[4].to_string(),
+        recipient: custom[2].to_string(),
+        channel_id: custom[2].to_string(), // reserved
+        alias: "".to_string(),             // reserved
+        id: params["txn_id"].to_string(),
+        address: params["receiver_email"].to_string(),
         processor: "PayPal".to_string(),
         amount: amount,
         currency: params["mc_currency"].to_string(),
-        target_currency: custom[2].to_string(),
+        target_currency: custom[5].to_string(),
         ts: ts,
     })
 }
@@ -146,21 +156,24 @@ mod tests {
 
     #[test]
     fn test_argument_parsing() {
-        let receipt = parse_ipn(&web::Query::from_query("mc_gross=100.00&invoice=.XXXXXXXXXX&protection_eligibility=Eligible&payer_id=XXXXXXXXX&payment_date=07%3A25%3A25+May+13%2C+2021+PDT&payment_status=Completed&charset=KOI8_R&first_name=XXXXXX&mc_fee=14.40&notify_version=3.9&custom=6451b474b8ed84b5ad2d6f834f454d9800341e0f04c9ae8e40b9911dffa38cbb+d40bebba-aa61-8b6a-62b6-cd265df42796+EUR&payer_status=verified&business=xxxxxxx%40gmail.com&quantity=1&verify_sign=XXXXXXXX.XXXXXXX&payer_email=XXXXXXX%40example.com&txn_id=XXXXXXXXXX&payment_type=instant&payer_business_name=XXXXXXXX&last_name=XXXXXXXX&receiver_email=XXXXXXXX%40example.com&payment_fee=&shipping_discount=0.00&receiver_id=XXXXXXXXXXX&insurance_amount=0.00&txn_type=web_accept&item_name=Donate+to+d40bebba-aa61-8b6a-62b6-cd265df42796&discount=0.00&mc_currency=RUB&item_number=&residence_country=AT&shipping_method=Default&transaction_subject=6451b474b8ed84b5ad2d6f834f454d9800341e0f04c9ae8e40b9911dffa38cbb+d40bebba-aa61-8b6a-62b6-cd265df42796+EUR&payment_gross=&ipn_track_id=XXXXXXXXX").unwrap());
-        assert_eq!(receipt.is_ok(), true);
-        let receipt = receipt.unwrap();
+        // One-time donation
+        let receipt = parse_ipn(&web::Query::from_query("mc_gross=100.00&invoice_number=213341354543524&protection_eligibility=Eligible&payer_id=XXXXXXXXX&payment_date=07%3A25%3A25+May+13%2C+2021+PDT&payment_status=Completed&charset=KOI8_R&first_name=XXXXXX&mc_fee=14.40&notify_version=3.9&payer_status=verified&business=xxxxxxx%40gmail.com&quantity=1&verify_sign=XXXXXXXX.XXXXXXX&payer_email=XXXXXXX%40example.com&txn_id=XXXXXXXXXX&payment_type=instant&payer_business_name=XXXXXXXX&last_name=XXXXXXXX&receiver_email=XXXXXXXX%40example.com&payment_fee=&shipping_discount=0.00&receiver_id=XXXXXXXXXXX&insurance_amount=0.00&txn_type=web_accept&transaction_subject=Donate+to+4855e1d3-ac4a-f6c4-8e03-f66001cef053+from+256bd4c260ee7d9554cf926a5120d0632b149f54a86ac65b660198b4c42c292d+EUR&discount=0.00&mc_currency=RUB&item_number=&residence_country=AT&shipping_method=Default&payment_gross=&ipn_track_id=XXXXXXXXX").unwrap()).unwrap();
+        assert_eq!(receipt.recipient, "4855e1d3-ac4a-f6c4-8e03-f66001cef053");
+        assert_eq!(receipt.channel_id, "4855e1d3-ac4a-f6c4-8e03-f66001cef053");
         assert_eq!(
             receipt.donor,
-            "6451b474b8ed84b5ad2d6f834f454d9800341e0f04c9ae8e40b9911dffa38cbb"
+            "256bd4c260ee7d9554cf926a5120d0632b149f54a86ac65b660198b4c42c292d"
         );
-        assert_eq!(receipt.recipient, "d40bebba-aa61-8b6a-62b6-cd265df42796");
-        assert_eq!(receipt.channel_id, "d40bebba-aa61-8b6a-62b6-cd265df42796");
-        assert_eq!(receipt.alias, "d40bebba-aa61-8b6a-62b6-cd265df42796");
-        assert_eq!(receipt.id, ".XXXXXXXXXX");
-        assert_eq!(receipt.address, "xxxxxxx@gmail.com");
+        assert_eq!(receipt.alias, "");
+        assert_eq!(receipt.id, "XXXXXXXXXX");
+        assert_eq!(receipt.address, "XXXXXXXX@example.com");
         assert_eq!(receipt.processor, "PayPal");
         assert_eq!(receipt.amount, 85.6);
         assert_eq!(receipt.currency, "RUB");
         assert_eq!(receipt.target_currency, "EUR");
+
+        // Recurring payment
+        let receipt = parse_ipn(&web::Query::from_query("mc_gross=2.00&period_type=+Regular&outstanding_balance=0.00&next_payment_date=03%3A00%3A00+May+13%2C+2022+PDT&protection_eligibility=Ineligible&payment_cycle=Monthly&tax=0.00&payer_id=QWRKD4DDU87H2&payment_date=03%3A21%3A05+Apr+13%2C+2022+PDT&payment_status=Completed&product_name=Donate+to+4855e1d3-ac4a-f6c4-8e03-f66001cef053+from+6451b474b8ed84b5ad2d6f834f454d9800341e0f04c9ae8e40b9911dffa38cbb+EUR&charset=UTF-8&recurring_payment_id=XXXXXXXXX&first_name=XXXXXXX&mc_fee=0.46&notify_version=3.9&amount_per_cycle=2.00&payer_status=verified&currency_code=EUR&business=donate%40smartlike.org&verify_sign=XXXXXXXXXXXXXXXXX&payer_email=XXXXXXXX%40example.com&initial_payment_amount=0.00&profile_status=Active&amount=2.00&txn_id=XXXXXX&payment_type=instant&payer_business_name=XXXXXXXs&last_name=XXXXXXX&receiver_email=donate%40smartlike.org&payment_fee=&receiver_id=XXXXXXX&txn_type=recurring_payment&mc_currency=EUR&residence_country=US&transaction_subject=Donate+to+4855e1d3-ac4a-f6c4-8e03-f66001cef053+from+256bd4c260ee7d9554cf926a5120d0632b149f54a86ac65b660198b4c42c292d+EUR&payment_gross=&shipping=0.00&product_type=1&time_created=07%3A45%3A05+Mar+13%2C+2022+PDT&ipn_track_id=XXXXXXXXX").unwrap()).unwrap();
+        assert_eq!(receipt.recipient, "4855e1d3-ac4a-f6c4-8e03-f66001cef053");
     }
 }
